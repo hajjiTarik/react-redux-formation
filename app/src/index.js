8c9b7cdd336a840510da8d61ed75bf752af27c97
Hajji Tarik <tarhajji@sqli.com>
2018-11-11 22:16:56 +0100

    init projet

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './style.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Welcome</h1>
+      </div>
+    );
+  }
+}
